91219a0b7025a4bdee44821ef7f9a21ba701efe1
fadhlna <nafadhil26@gmail.com>
2024-09-10 08:38:37 +0700

    First Github Project

--- app.py
+++ app.py
@@ -6,3 +6,19 @@ def luas_segitiga():
     print("Luas segitiga adalah: ", luas)
 
 luas_segitiga()
+
+# Hitung luas persegi panjang
+def luas_persegi_panjang():
+    a = int(input("masukkan panjang persegi: "))
+    t = int(input("Masukkan lebar persegi: "))
+    luas = p * 1
+    print("Luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
+
+# Hitung luas lingkaran
+def luas_lingkaran():
+    a = int(input("Masukkan jari jari lingkaran: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas 
+   
